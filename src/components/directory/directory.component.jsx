48f199e0e4b1657ddfb5bdd38d27de425c7a9bab
Sagar Dalvi <dalvi.sagar.b@gmail.com>
2021-07-30 17:54:54 +0530

    Lesson 3 - Styling menu item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,32 +10,32 @@ class Directory extends React.Component {
       sections: [
         {
           title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          imageUrl: 'https://i.ibb.co/Br2Gpxc/hats.png',
           id: 1,
           linkUrl: 'shop/hats'
         },
         {
           title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          imageUrl: 'https://i.ibb.co/7WqXtNh/jackets.png',
           id: 2,
           linkUrl: 'shop/jackets'
         },
         {
           title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          imageUrl: 'https://i.ibb.co/s5Kny0L/sneakers.png',
           id: 3,
           linkUrl: 'shop/sneakers'
         },
         {
           title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          imageUrl: 'https://i.ibb.co/kmzr1x9/womens.png',
           size: 'large',
           id: 4,
           linkUrl: 'shop/womens'
         },
         {
           title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          imageUrl: 'https://i.ibb.co/9Wdvkgx/men.png',
           size: 'large',
           id: 5,
           linkUrl: 'shop/mens'
