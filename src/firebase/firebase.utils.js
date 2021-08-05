43359f5b895b27fc906bd90867e85e5ab249402b
Sagar Dalvi <dalvi.sagar.b@gmail.com>
2021-08-05 18:04:16 +0530

    Storing Google signed in User data in firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,32 @@ const config = {
   appId: "1:548446852075:web:20b4ef7a34e67bb549ffae"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const {displayName, email, photoURL} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        photoURL,
+        ...additionalData
+      })
+    } catch (e) {
+      console.log('Error creating user', e);
+    }
+  }
+  return userRef;
+
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
