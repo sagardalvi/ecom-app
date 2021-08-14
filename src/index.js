a86631f77b1f59cd3cc5ffbdc2c3a89abb966a5e
Sagar Dalvi <dalvi.sagar.b@gmail.com>
2021-08-14 14:32:43 +0530

    Added Redux and implemented user reducer and action in header and app component

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App/>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
