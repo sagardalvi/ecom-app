2b8946281bab13ea6440a078d302b10753485fae
Sagar Dalvi <dalvi.sagar.b@gmail.com>
2021-08-20 17:19:33 +0530

    Added add and remove item functionality to checkout page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Redirect, Route, Switch} from 'react-router-dom';
 import './App.scss';
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import {connect} from 'react-redux';
@@ -45,6 +47,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route exact path='/shop' component={ShopPage}/>
+          <Route exact path='/checkout' component={CheckoutPage}/>
           <Route exact path='/signin'
                  render={() => this.props.currentUser ? <Redirect to={'/'}/> : <SignInAndSignUpPage/>}/>
         </Switch>
